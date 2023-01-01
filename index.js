6a5d06337a0eddbb0d3477a257662ef761cfc361
Kent Domaoal <kentpaulnoeldomaoal@gmail.com>
2023-01-01 15:52:08 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const NewmanConfig = require('./lib/NewmanConfig.js')
+
+if (process.argv.length <= 2) {
+    console.log("Give a feed file to proces and run the tests")
+} else {
+    var args = process.argv.slice(2);
+    new NewmanConfig(args[0])
+}
